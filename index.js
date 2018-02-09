e2fcf1715fe290b7ebf9f409dbdf4257106d567d
sergey <sramator@gmail.com>
2018-02-09 12:44:20 +0200

    #4 - Project setup

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const [ arg ] = process.argv.slice(2);
+const [arg] = process.argv.slice(2);
 
-if (arg === '--version') {
-  console.log('v0.0.1');
+if (arg === `--version`) {
+  console.log(`v0.0.1`);
   process.exit(0);
 }
 
-if (arg === '--help') {
-  console.log('--help — печатает этот текст');
-  console.log('--version — печатает версию приложения)');
+if (arg === `--help`) {
+  console.log(`--help — печатает этот текст`);
+  console.log(`--version — печатает версию приложения)`);
   process.exit(0);
 }
 
 if (!arg) {
-  console.log('Привет пользователь! Эта программа будет запускать сервер Keksobooking.');
-  console.log('Автор: Кекс.');
+  console.log(`Привет пользователь! Эта программа будет запускать сервер Keksobooking.`);
+  console.log(`Автор: Кекс.`);
   process.exit(0);
 }
 
